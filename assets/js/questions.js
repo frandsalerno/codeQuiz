cdf44410ff583aa61c572640e564c6fc99015129
Fran Salerno <salerno.fran@gmail.com>
2022-12-04 10:10:59 +0000

    Logic partially done

---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+//The array of objects with the questions
+var questions = [
+    {
+        title: 'What is the acronym for the Document Object Model',
+        choices: ['OBJ', 'KLM', 'DOM', 'CSS'],
+        answer: 'DOM'
+    },
+    {
+        title: 'What is the useful JS tool for DRYing up code',
+        choices: ['localStorage', 'strings', 'comments', 'functions'],
+        answer: 'functions'
+    } 
+]
+
+
+
